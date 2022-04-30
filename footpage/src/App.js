e8532258b0e7dff9c943c439aa7ad890d431f6cd
poyelana <segmogul@gmail.com>
2022-04-30 06:33:19 -0700

    first commit

---
+++ footpage/src/App.js
@@ -0,0 +1,23 @@
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
+export default App;
